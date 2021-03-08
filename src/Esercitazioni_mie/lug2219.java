57121077b487ea8dc50e9a651bbe0d5f053abdee
Mattia <giumatt99@gmail.com>
2021-03-08 10:19:16 +0100

    c

--- src/Esercitazioni_mie/lug2219.java
+++ src/Esercitazioni_mie/lug2219.java
@@ -1,12 +1,12 @@
-package Esercitazioni_mie;
+/*package Esercitazioni_mie;
 
-import alberi.AlberoBin;
+import alberi.AlberoBin;*/
 
 /*Si deve realizzare un metodo ricorsivo che restituisce true se e solo se esiste in a al livello l almeno un nodo foglia n
 che è figlio unico e tale che n.val() è contenuto in almeno un altro nodo che si trova a un qualsiasi livello l1 tale che 
 l1 != l.*/
 
-public class lug2219 {
+/*public class lug2219 {
     
     public static boolean verifica(AlberoBin<Integer> a, int l) {
         if(a == null) return false;
@@ -21,4 +21,4 @@ public class lug2219 {
     }
 
 
-}
+}*/
