55863617d1c11bbe8c8030184e0124fdd5846d20
Giuseppe Mattia Greco <giumatt99@gmail.com>
2021-03-17 16:47:56 +0100

    aggiunta esercizi

--- src/Esercitazioni_mie/esercitazioniVarie.java
+++ src/Esercitazioni_mie/esercitazioniVarie.java
@@ -91,4 +91,92 @@ public class esercitazioniVarie {
                 return true;
         return nodi(a.sin(), radice) && nodi(a.des(), radice);
     }
+
+    //Realizzare un metodo che restituisce true se e solo se l'albero contiene un valore x pari compreso nell'intervallo [valMin, valMax], con valMin <= valMax.
+    public static boolean intervallo(AlberoBin<Integer> a, int valMin, int valMax) {
+        if(a == null) return false;
+        if((a.getVal() >= valMin && a.getVal() <= valMax) && a.getVal() % 2 == 0) return true;
+        return intervallo(a.sin(), valMin, valMax) || intervallo(a.des(), valMin, valMax);
+    }
+
+    /*
+    Complessità temporale migliore Theta(1)
+    Complessità temporale peggiore Theta(n)
+
+    Complessità spaziale migliore Theta(1)
+    Complessità spaziale peggiore Theta(n)
+    */
+
+    //Il metodo restituisce true se e solo se i valori contenuti nell'albero a sono contenuti anche nell'albero b.
+    public static boolean contenuto(AlberoBin<Integer> a, AlberoBin<Integer> b) {
+        if(a == null && b == null) return true;
+        if(confronta(a.getVal(), b)) return true;
+        return contenuto(a.sin(), b) && contenuto(a.des(), b);
+    }
+
+    public static boolean confronta(int x, AlberoBin<Integer> b) {
+        if(b == null) return false;
+        if(b.getVal() == x) return true;
+        return confronta(x, b.sin()) || confronta(x, b.des());
+    }
+    /*
+    Complessita temporale migliore Theta(n)
+    Comolessità temporale peggiore Theta(n * m)
+
+    Complessità spaziale migliore Theta(n)
+    Complessità spaziale peggiore Theta(n + m)
+    */
+
+    //Il metodo restituisce true se e solo se la parte informativa di tutti i nodi foglia dell'albero a è maggiore di k
+    public static boolean foglie(AlberoBin<Integer> a, int k) {
+        if(a == null) return false;
+        if(a.sin() == null && a.des() == null) return a.getVal() > k;
+        return foglie(a.sin(), k) && foglie(a.des(), k);
+    }
+
+    /*
+    Complessità temporale migliore Theta(1)
+    Complessità temporale peggiore Theta(n)
+
+    Complessità spaziale migliore Theta(1)
+    Complessità spaziale peggiore Theta(n)
+    */
+
+    //Il metodo restituisce true se e solo se vi è almeno un nodo n tale che l'intero x appare sia nel sottoalbero sinstro che nel sottoalbero destro di n,
+    public static boolean ver3(AlberoBin<Integer> a, int x) {
+        if(a == null) return false;
+        return metodo(a, x) >= 1;
+    }
+
+    public static int metodo(AlberoBin<Integer> a, int x) {
+        if(a == null) return 0;
+        if (a.sin() != null || a.des() != null)
+            return metodo(a.sin(), x) + metodo(a.des(), x);
+        if(a.getVal() == x)
+            return 1;
+        return 0;
+    }
+
+    /*
+    Complessità spaziale migliore Theta(1)
+    Complessità spaziale peggiore Theta(n^2)
+
+    Complessità temporale migliore Theta(1)
+    Complessità temporale peggiore Theta(n)
+    */
+
+    //Il metodo restituisce true se e solo se un dato valore v compare alla stessa profondità sia nell'albero a che nell'albero b.
+    public static boolean profondita(AlberoBin<Integer> a, AlberoBin<Integer> b, int v) {
+        if(a == null && b == null) return false;
+        if(a.getVal() == v && b.getVal() == v) return true;
+        return profondita(a.sin(), b.des(), v) || profondita(a.des(), b.des(), v) || profondita(a.sin(), b.des(), v) || profondita(a.des(), b.sin(), v);
+    }
+
+    /*
+    Complessità spaziale migliore Theta(n)
+    Complessità spaziale peggiore Theta(n * m)
+
+    Complessità temporale migliore Theta(n)
+    Complessità temporale peggiore Theta(n + m)
+    */
 }
